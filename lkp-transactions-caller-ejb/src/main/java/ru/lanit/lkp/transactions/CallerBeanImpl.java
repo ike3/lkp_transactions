350a8dfb12e7b520553b6e94dd92e035756c160d
Sergey Kuzmin <sergey.kuzmin@artezio.com>
2018-12-11 10:00:18 +0300

    FCSNF-6983 Вызов callee сервиса из caller

--- lkp-transactions-caller-ejb/src/main/java/ru/lanit/lkp/transactions/CallerBeanImpl.java
+++ lkp-transactions-caller-ejb/src/main/java/ru/lanit/lkp/transactions/CallerBeanImpl.java
@@ -11,8 +11,13 @@ public class CallerBeanImpl implements CallerBean {
     @Inject
     private SomeDao dao;
 
+    @Inject
+    private CalleeService callee;
+
     @Override
     public String doSomething(String parameter) {
+        callee.doSomething(parameter);
+
         dao.logJournal("Caller is doing something with " + parameter);
         return "done with " + parameter;
     }
