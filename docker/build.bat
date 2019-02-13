26918873c6d4ce967cb6af16697463c73f0e1adc
Sergey Kuzmin <sergey.kuzmin@artezio.com>
2019-02-13 14:59:14 +0300

    Добавил проксю для координатора, чтобы логировать запросы к нему

--- docker/build.bat
+++ docker/build.bat
@@ -2,3 +2,17 @@ docker build --tag=lkp_transactions_caller caller
 docker build --tag=lkp_transactions_callee callee
 docker build --tag=lkp_transactions_coordinator coordinator
 docker build --tag=lkp_transactions_nginx nginx
+
+docker build --tag=lkp_transactions_logproxy logproxy
+
+rem
+rem Просмотреть логи
+rem 
+rem docker exec -it docker_caller_1 cat /opt/jboss/wildfly/standalone/configuration/standalone.xml
+rem 
+
+rem
+rem Просмотреть пакеты 
+rem 
+rem docker exec -it docker_callee_1 cat /tmp/bodies/0000000001
+rem 
