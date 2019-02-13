31e230118116cf1f2143bb38a8cb20a46ad62799
Sergey Kuzmin <sergey.kuzmin@artezio.com>
2019-02-13 11:39:18 +0300

    Отдельный координатор транзакций

---
+++ docker/build.bat
@@ -0,0 +1,4 @@
+docker build --tag=lkp_transactions_caller caller
+docker build --tag=lkp_transactions_callee callee
+docker build --tag=lkp_transactions_coordinator coordinator
+docker build --tag=lkp_transactions_nginx nginx
