6c62343a419ac125a0648f6f38aa793ef2451076
Sergey Kuzmin <sergey.kuzmin@artezio.com>
2019-02-25 10:07:31 +0300

    Переделал на ejb-api с ejb-client (первый слишком криво работает)

--- lkp-transactions-callee-ejb-api/src/main/java/ru/lanit/lkp/transactions/Callee.java
+++ lkp-transactions-callee-ejb-api/src/main/java/ru/lanit/lkp/transactions/Callee.java
@@ -1,5 +1,8 @@
 package ru.lanit.lkp.transactions;
 
+import javax.ejb.Local;
+
+@Local
 public interface Callee {
     String doSomething(String parameter);
 }
--- lkp-transactions-caller-ejb-api/src/main/java/ru/lanit/lkp/transactions/Caller.java
+++ lkp-transactions-caller-ejb-api/src/main/java/ru/lanit/lkp/transactions/Caller.java
@@ -1,5 +1,8 @@
 package ru.lanit.lkp.transactions;
 
+import javax.ejb.Local;
+
+@Local
 public interface Caller {
     String doSomething(String parameter);
 }
